6965cba0a0e14f9a16416f0f34f462a69caa2682
Logan Villard <logan.villard.pro@gmail.com>
2024-04-30 16:07:26 +0200

    Initialisation du projet

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pyxel
+
+class Jeu:
+    def __init__(self) -> None:
+        pyxel.init(128, 128)
+        pyxel.run(self.update, self.draw)
+    
+    
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+
+Jeu()
