e8221882ed3203d117d78928532a6dc41e759c0c
Logan Villard <logan.villard.pro@gmail.com>
2024-04-30 17:09:52 +0200

    Changements de pages

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 import pyxel
+import os
 
 class Jeu:
+    pyxel.init(256, 256, caption = "Mini JO")
     def __init__(self) -> None:
-        pyxel.init(128, 128)
+        pyxel.mouse(True)
+        
+        self.bouton = Bouton(0, 0, 100, 100, 9, "Test", Accueil)
+        self.boutons = [self.bouton]
+        
         pyxel.run(self.update, self.draw)
     
     
     def update(self):
-        pass
+        print("a")
+        for bouton in self.boutons:
+            bouton.clique()
     
     def draw(self):
-        pass
+        pyxel.rect(0, 0, 256, 256, 7)
+        for bouton in self.boutons:
+            bouton.dessiner()
 
+class Accueil:
+    def __init__(self):
+        self.bouton = Bouton(0, 0, 100, 100, 8, "Test", Jeu)
+        self.boutons = [self.bouton]
+        pyxel.run(self.update, self.draw)
+        
+    def update(self):
+        print("b")
+        for bouton in self.boutons:
+            bouton.clique()
+
+    def draw(self):
+        pyxel.rect(0, 0, 256, 256, 7)
+        for bouton in self.boutons:
+            bouton.dessiner()
+class Bouton:
+    def __init__(self, x, y, w, h, couleur, texte, action = None) -> None:
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.couleur = couleur
+        self.texte = texte
+        self.action = action
+    
+    def dessiner(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, self.couleur)
+        pyxel.text(self.w / 2 - 7, self.h / 2 - 5, self.texte, 0)
+
+    def clique(self):
+        if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
+            if pyxel.mouse_x > self.x and pyxel.mouse_x < self.x + self.w and pyxel.mouse_y > self.y and pyxel.mouse_y < self.y + self.h:
+                if self.action != None:
+                    self.action()
+                print("cliqué")
 Jeu()
